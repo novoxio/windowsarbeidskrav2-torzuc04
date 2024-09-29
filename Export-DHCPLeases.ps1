a6cbcaa3b4ec26eb3ca4d05f4fe7801e5cba044d
novoxio <79159505+novoxio@users.noreply.github.com>
2024-09-29 05:04:45 -0700

    Add files via upload

---
+++ Export-DHCPLeases.ps1
@@ -0,0 +1,24 @@
+﻿# Define the CSV file path where the leases will be exported
+$csvFilePath = "C:\Users\torzuc04\Documents\DHCPLeases.csv"
+
+# Get all DHCP leases
+$leases = Get-DhcpServerv4Lease
+
+# Create an array to store lease data with required fields
+$leaseData = @()
+
+# Loop through each lease and store the relevant information
+foreach ($lease in $leases) {
+    $leaseData += New-Object PSObject -Property @{
+        "ScopeId"      = $lease.ScopeId
+        "IPAddress"    = $lease.IPAddress
+        "HostName"     = $lease.HostName
+        "ClientID"     = $lease.ClientId
+        "AddressState" = $lease.AddressState
+    }
+}
+
+# Export the lease data to a CSV file with semicolon as the delimiter
+$leaseData | Export-Csv -Path $csvFilePath -Delimiter ';' -NoTypeInformation
+
+Write-Host "DHCP leases have been exported to $csvFilePath."
